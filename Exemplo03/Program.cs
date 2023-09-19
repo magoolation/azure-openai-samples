7101d8405b6b6785d02e797457b30754718c2060
Alexandre Costa <alexandresantoscosta@gmail.com>
2023-09-19 09:46:36 -0300

    Exemplo 03: Chat History

---
+++ Exemplo03/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.UserSecrets;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.AI.ChatCompletion;
+
+var config = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+string aoaiEndpoint = config["AZUREOPENAI_ENDPOINT"]!;
+string aoaiApiKey = config["AZUREOPENAI_API_KEY"]!;
+string aoaiModel = "gpt35turbo";
+
+// Initialize the kernel
+IKernel kernel = Kernel.Builder
+    .WithAzureChatCompletionService(aoaiModel, aoaiEndpoint, aoaiApiKey)
+    .Build();
+
+// Create a new chat
+IChatCompletion ai = kernel.GetService<IChatCompletion>();
+ChatHistory chat = ai.CreateNewChat("You are an AI assistant that helps people find information.");
+
+// Q&A loop
+while (true)
+{
+    Console.Write("Question: ");
+    chat.AddUserMessage(Console.ReadLine()!);
+
+    string answer = await ai.GenerateMessageAsync(chat);
+    chat.AddAssistantMessage(answer);
+    Console.WriteLine(answer);
+
+    Console.WriteLine();
+}
